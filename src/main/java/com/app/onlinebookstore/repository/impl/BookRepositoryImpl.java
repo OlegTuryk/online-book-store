e6a01036a45e27ebf432529b14f292794e38d70a
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-09-02 19:40:16 +0300

    Configured pom.xml, application.properties, created Book model, Book

---
+++ src/main/java/com/app/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.app.onlinebookstore.repository.impl;
+
+import com.app.onlinebookstore.model.Book;
+import com.app.onlinebookstore.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert book " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Book ", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books ", e);
+        }
+    }
+}
