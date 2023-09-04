e02385416de859130d5736ccadc17231d8e29a69
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-09-04 18:08:28 +0300

    Fixed errors with exceptions

--- src/main/java/com/app/onlinebookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/app/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -28,7 +29,7 @@ public class BookRepositoryImpl implements BookRepository {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't insert book " + book, e);
+            throw new DataAccessResourceFailureException("Can't insert book " + book, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -41,7 +42,7 @@ public class BookRepositoryImpl implements BookRepository {
         try (Session session = sessionFactory.openSession()) {
             return session.createQuery("FROM Book ", Book.class).getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can't get all books ", e);
+            throw new DataAccessResourceFailureException("Can't get all books ", e);
         }
     }
 }
