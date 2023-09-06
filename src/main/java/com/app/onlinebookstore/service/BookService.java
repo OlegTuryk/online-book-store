6688d1b9cd6fae159285fbdb749d87d80ada22eb
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-09-06 10:19:43 +0300

    Create BookDto, CreateBookRequestDto, BookController, EntityNotFoundException, BookMapper, MapperConfig, update BookService

--- src/main/java/com/app/onlinebookstore/service/BookService.java
+++ src/main/java/com/app/onlinebookstore/service/BookService.java
@@ -1,10 +1,13 @@
 package com.app.onlinebookstore.service;
 
-import com.app.onlinebookstore.model.Book;
+import com.app.onlinebookstore.dto.BookDto;
+import com.app.onlinebookstore.dto.CreateBookRequestDto;
 import java.util.List;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto requestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto getBookById(Long id);
 }
