515d6fc1392a5f3aeac174a722877ee9cac5dddb
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-09-11 08:50:57 +0300

    added CustomGlobalExceptionHandler

--- src/main/java/com/app/onlinebookstore/controller/BookController.java
+++ src/main/java/com/app/onlinebookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import com.app.onlinebookstore.dto.BookDto;
 import com.app.onlinebookstore.dto.BookSearchParametersDto;
 import com.app.onlinebookstore.dto.CreateBookRequestDto;
 import com.app.onlinebookstore.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,7 +25,7 @@ public class BookController {
     private final BookService bookService;
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
