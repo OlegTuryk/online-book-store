bce2fce0ecfd25127caf79b3236f4acfc4a874d2
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-09-27 10:03:07 +0300

    implemented customMysqlContainer

---
+++ src/test/java/com/app/onlinebookstore/config/repository/BookRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.app.onlinebookstore.config.repository;
+
+import com.app.onlinebookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+
+}
